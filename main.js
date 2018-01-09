0cb6dfa8c7280b46a3bdb3e2776b05c7bfeb1dc4
Leonardo Matos <leomp120894@gmail.com>
2018-01-09 09:58:12 -0200

    better error logs

--- main.js
+++ main.js
@@ -11,8 +11,20 @@
 process.on('uncaughtException', (err) => {
   // fatal error
   // log to file before exit
+  let msg = '\n[' + new Date().toString() + ']\n'
+  if (err) {
+    if (err.hasOwnProperty('stack')) {
+      msg += err.stack
+    } else if (err.hasOwnProperty('message')) {
+      msg += err.message
+    } else {
+      msg += err.toString()
+    }
+    msg += '\n'
+  }
+
   let fs = require('fs')
-  fs.appendFile('/var/log/nodejs/_stderr', '\n' + err.stack + '\n', () => {
+  fs.appendFile('/var/log/nodejs/_stderr', msg, () => {
     process.exit(1)
   })
 })
